387526f200a6761c0bffc8346e67d320caca3064
bwaddell <benwaddell@gmail.com>
2018-02-26 14:15:36 -0700

    WebPages, EvaluateEvent, Theme

---
+++ RateSite/EvaluateEvent.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="EvaluateEvent.aspx.cs" Inherits="EvaluateEvent" Theme="ContinUI" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainBody" Runat="Server">
+
+        <div class="btnClass">
+            <asp:Button ID="ButtonUp" runat="server" Text="Up" OnClick="ButtonUp_Click"/>
+        </div>
+
+
+        <div class="RatingClass">
+            <asp:Label ID="LabelRating" runat="server" Text="5"></asp:Label>
+        </div>
+
+        <div class="btnClass block">
+            <asp:Button ID="ButtonDown" runat="server" Text="Down" OnClick="ButtonDown_Click" />
+        </div>
+
+
+    
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Scripts" Runat="Server">
+</asp:Content>
+
---
+++ RateSite/EvaluateEvent.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class EvaluateEvent : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ButtonUp_Click(object sender, EventArgs e)
+    {
+        int Rating = int.Parse(LabelRating.Text);
+        Rating = (Rating + 1 > 10)  ?  Rating = 10 : Rating + 1;
+
+        LabelRating.Text = Rating.ToString();
+
+
+        Evaluation eval = new Evaluation();
+
+        eval.Rating = Rating;
+    }
+
+    protected void ButtonDown_Click(object sender, EventArgs e)
+    {
+        int Rating = int.Parse(LabelRating.Text);
+        Rating = (Rating - 1 < 1) ? Rating = 1 : Rating - 1;
+
+        LabelRating.Text = Rating.ToString();
+
+        Evaluation eval = new Evaluation();
+
+        eval.Rating = Rating;
+    }
+
+
+}
