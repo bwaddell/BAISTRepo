531753251e46ec80d2242425b7609c5e2ba80e92
cjacob1 <cjacob1@WA304-13>
2018-02-28 16:05:03 -0700

    Analyze Event test data

--- RateSite/AnalyzeEvent.aspx.cs
+++ RateSite/AnalyzeEvent.aspx.cs
@@ -4,27 +4,53 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.DataVisualization.Charting;
+
 
 public partial class AnalyzeEvent : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+            lbStartTime.Text = "<br /> Page Loaded at: " + DateTime.Now.ToString();
+
+        CSS Director = new CSS();
+        Evaluation eval = new Evaluation();
+
+
+        //tbEventID.Text
+        //Director.GetEvaluationData()
+
+        Random rnd = new Random();
+
+        DateTime[] x = new DateTime[20];
+        int[] y = new int[20]; //{ 0, 1, 2, 3, 4, 4, 5, 5, 8, 9 };
+
+        for (int i = 0; i < 20; i++)
+        {
+            x[i] = DateTime.UtcNow.AddDays(i);
+            y[i] = rnd.Next(1, 10);
+        }
+
+
+        Chart1.Series[0].Points.DataBindXY(x, y);
+        Chart1.Series[0].ChartType = SeriesChartType.Line;
         
-        lbStartTime.Text = "<br /> page load at " + DateTime.Now.ToString();
+
+        //Chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+
+        //Chart1.Legends[0].Enabled = true;
 
     }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        CSS Director = new CSS();
-        Evaluation eval = new Evaluation(100, 1, 1);
 
-        lbCurrentTime.Text += "<br />" + Director.AddEvaluation(eval).ToString();
     }
 
 
     protected void TimerForGraphRefresh_Tick(object sender, EventArgs e)
     {
-        lbCurrentTime.Text = "<br /> refereshed at " + DateTime.Now.ToString();
+        lbUpdateTime.Text = "Update Time: " + DateTime.Now.ToString();
     }
 }
