3b35f4520f082a595060a81786fc198770b27470
bwaddell <benwaddell@gmail.com>
2018-03-19 13:16:24 -0600

    more facilitator authentication

---
+++ RateSite/App_Code/FacilitatorDirector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for FacilitatorDirector
+/// </summary>
+public class FacilitatorDirector
+{
+    public FacilitatorDirector()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public Facilitator GetFacilitatorByEmail(string email)
+    {
+        Facilitator pullFacilitator = new Facilitator();
+
+
+        return pullFacilitator;
+    }
+}
